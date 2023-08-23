8df7fbf822ec50ac09e40fb9b32c08b24bcbf673
Gabriel Barboza <65047738+gabrieIbarboza@users.noreply.github.com>
2023-08-23 20:18:23 -0300

    ✨ practicing pointers again

---
+++ c5-pointers/act2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void somaCem(int *v);
+
+int main()
+{
+    int v1, v2, *p_v1, *p_v2;
+
+    printf("Digite um valor inteiro: ");
+    scanf(" %d", &v1);
+    fflush(stdin);
+    printf("Digite outro valor inteiro: ");
+    scanf(" %d", &v2);
+
+    p_v1 = &v1;
+    p_v2 = &v2;
+
+    if(p_v1 > p_v2)
+    {
+        printf("\n\nO maior endereco e: %p e seu valor e: %d", p_v1, *p_v1);
+    }
+    else
+    {
+        printf("\n\nO maior endereco e: %p e seu valor e: %d", p_v2, *p_v2);
+    }
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
+
+void somaCem(int *v)
+{
+    *v += 100;
+}
