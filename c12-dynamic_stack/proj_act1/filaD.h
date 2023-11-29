354feea1b9c5124e74f3a7813071f7b1006c8867
gabrieibarboza <gabrielklarkgk@gmail.com>
2023-11-28 21:40:03 -0300

    ✨ 12th class

---
+++ c12-dynamic_stack/proj_act1/filaD.h
@@ -0,0 +1,23 @@
+typedef struct aluno{
+    int matricula;
+    float n1, n2, n3;
+}ALUNO;
+
+typedef struct fila Fila;
+
+
+Fila *cria_fila();
+
+void destroi_fila(Fila *fi);
+
+int tamanho_fila(Fila *fi);
+
+int fila_cheia(Fila *fi);
+
+int fila_vazia(Fila *fi);
+
+int insere_fila(Fila *fi, ALUNO al);
+
+int remove_fila(Fila *fi);
+
+int consulta_fila(Fila *fi, ALUNO *al);
---
+++ c12-dynamic_stack/proj_act2/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilhaD.h"
+
+int main()
+{
+    int x; // para os codigos de erros
+    ALUNO al, al1, al2, al3;
+    Pilha *pi;
+
+    pi = cria_pilha();
+
+    al1.matricula = 100;
+    al1.n1 = 8.3;
+    al1.n2 = 8.4;
+    al1.n3 = 8.5;
+
+    al2.matricula = 110;
+    al2.n1 = 7.3;
+    al2.n2 = 7.4;
+    al2.n3 = 7.5;
+
+    al3.matricula = 120;
+    al3.n1 = 6.3;
+    al3.n2 = 6.4;
+    al3.n3 = 6.5;
+
+    x = tamanho_pilha(pi);
+    printf("\nO tamanho da pilha e: %d", x);
+
+     x = pilha_cheia(pi);
+    if(x){
+        printf("\nA pilha esta cheia!");
+    }else {
+        printf("\nA pilha nao esta cheia");
+    }
+
+    x = pilha_vazia(pi);
+    if(x){
+        printf("\nA pilha esta vazia!");
+    }else {
+        printf("\nA pilha nao esta vazia");
+    }
+
+
+    x = insere_pilha(pi, al1);
+    if(x){
+        printf("\nElemento inserido com sucesso!");
+    } else {
+        printf("\nErro, elemento nao inserido.");
+    }
+
+    x = insere_pilha(pi, al2);
+    if(x){
+        printf("\nElemento inserido com sucesso!");
+    } else {
+        printf("\nErro, elemento nao inserido.");
+    }
+
+    x = insere_pilha(pi, al3);
+    if(x){
+        printf("\nElemento inserido com sucesso!");
+    } else {
+        printf("\nErro, elemento nao inserido.");
+    }
+
+    x = remove_pilha(pi);
+    if(x){
+        printf("\nElemento removido com sucesso!");
+    } else {
+        printf("\nErro, elemento nao removido.");
+    }
+
+    x = consulta_pilha(pi, &al);
+    if(x){
+        printf("\nConsulta realizada com sucesso: ");
+        printf("\nMatricula: %d", al.matricula);
+        printf("\nNota 1: %.2f", al.n1);
+        printf("\nNota 2: %.2f", al.n2);
+        printf("\nNota 3: %.2f", al.n3);
+    }else {
+        printf("\nErro, consulta nao realizada.");
+    }
+
+    destroi_pilha(pi);
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
