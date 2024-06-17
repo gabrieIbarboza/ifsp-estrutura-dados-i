119480e9284c6bb82b7cf56a5e32b8799c5b5630
gabrieibarboza <gabrielklarkgk@gmail.com>
2024-06-16 22:53:42 -0300

    🚀 complete 25th class

--- c25-avl_tree/act1/arvoreAVL.h
+++ c25-avl_tree/act1/arvoreAVL.h
@@ -1,7 +1,22 @@
 typedef struct NO *arvAVL;
 
+arvAVL *cria_arvAVL();
+void liberar_arvAVL(arvAVL *raiz);
+
 void rotacaoLL(arvAVL *A);
 void rotacaoRR(arvAVL *A);
-
 void rotacaoLR(arvAVL *A);
 void rotacaoRL(arvAVL *A);
+
+int vazia_arvAVL(arvAVL *raiz);
+int altura_arvAVL(arvAVL *raiz);
+int totalNO_arvAVL(arvAVL * raiz);
+
+void preOrdem_arvAVL(arvAVL *raiz);
+void emOrdem_arvAVL(arvAVL *raiz);
+void posOrdem_arvAVL(arvAVL *raiz);
+
+void confirmaInsercao(int x);
+int insere_arvAVL(arvAVL *raiz, int valor);
+int remove_arvAVL(arvAVL *raiz, int valor);
+int consulta_arvAVL(arvAVL *raiz, int valor);
--- c25-avl_tree/act1/main.c
+++ c25-avl_tree/act1/main.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 #include "arvoreAVL.h"
 
-int main(){
-    int x;
-    arvAVL *raiz;
+int main()
+{
+
+    int x, valor;
+    arvAVL *raiz = cria_arvAVL();
+    confirmaInsercao(insere_arvAVL(raiz, 160));
+    confirmaInsercao(insere_arvAVL(raiz, 150));
+    confirmaInsercao(insere_arvAVL(raiz, 100));
+    confirmaInsercao(insere_arvAVL(raiz, 110));
+    confirmaInsercao(insere_arvAVL(raiz, 130));
+    confirmaInsercao(insere_arvAVL(raiz, 140));
+    confirmaInsercao(insere_arvAVL(raiz, 120));
+    confirmaInsercao(insere_arvAVL(raiz, 170));
+    confirmaInsercao(insere_arvAVL(raiz, 180));
+    confirmaInsercao(insere_arvAVL(raiz, 190));
+    confirmaInsercao(insere_arvAVL(raiz, 200));
+    confirmaInsercao(insere_arvAVL(raiz, 200));
+
+    printf("Digite um numero para deletar: \n");
+    scanf("%d", &valor);
+    x = remove_arvAVL(raiz, valor);
+    if(x){
+        printf("Elemento removido com sucesso!");
+    }else{
+        printf("Erro, nao foi possivel remover o elemento");
+    }
+
+    printf("\n\n");
+    system("pause");
+    return 0;
 }
